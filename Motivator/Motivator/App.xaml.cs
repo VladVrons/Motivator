dd299e66eee118168ab742b030202fb2a54d5b17
VladVrons <slava.pro1605@gmail.com>
2021-01-18 13:12:03 +0200

    It works again

--- Motivator/Motivator/App.xaml.cs
+++ Motivator/Motivator/App.xaml.cs
@@ -7,20 +7,18 @@ namespace Motivator
 {
     public partial class App : Application
     {
+     
         public App()
         {
             InitializeComponent();
             MainPage = new MainPage();
-
-            
         }
-        
 
         protected override void OnStart()
         {
-
-        }
-
+            
+        } 
+    
         protected override void OnSleep()
         {
             
